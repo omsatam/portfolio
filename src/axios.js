503f8c6bded2e1774727366a404b3573c623b26e
om satam <62635289+omsatam@users.noreply.github.com>
2023-03-13 12:55:41 +0530

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -3,6 +3,6 @@ import axios from "axios";
 // base url to make requests to create movies database
 
 const instance = axios.create({
-  baseURL: "https://portfolio-om.herokuapp.com/",
+  baseURL: "https://raw.githubusercontent.com/omsatam/portfolio/main/server/",
 });
 export default instance;
